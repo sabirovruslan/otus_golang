c6a8ae23f9b00bf5571a25200610f1cfa4048747
Ruslan <ruslan.sabirov@alao.ch>
2022-11-04 21:43:21 +0400

    HW2 Add tests for space

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -12,6 +12,7 @@ func TestUnpack(t *testing.T) {
 		expected string
 	}{
 		{input: "", expected: ""},
+		{input: " ", expected: " "},
 		{input: "a", expected: "a"},
 		{input: "aa", expected: "aa"},
 		{input: "abc", expected: "abc"},
@@ -22,11 +23,15 @@ func TestUnpack(t *testing.T) {
 		{input: "a3", expected: "aaa"},
 		{input: "a-1", expected: "a-"},
 		{input: "a-0", expected: "a"},
+		{input: " 0", expected: ""},
+		{input: " 1", expected: " "},
+		{input: " 2", expected: "  "},
 		{input: "a2a", expected: "aaa"},
 		{input: "a3a", expected: "aaaa"},
 		{input: "a3c®", expected: "aaac®"},
 		{input: "abc4a2", expected: "abccccaa"},
 		{input: "a0b0c0d0", expected: ""},
+		{input: "a w3 a", expected: "a www a"},
 	}
 
 	for _, tc := range tests {
