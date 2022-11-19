42ccd1da398c1c041c259528f828f286288d1c78
Ruslan <ruslan.sabirov@alao.ch>
2022-11-19 20:48:54 +0400

    HW04 Implementation cashe

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,6 +2,10 @@ package hw04lrucache
 
 type Key string
 
+func (k Key) String() string {
+	return string(k)
+}
+
 type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
@@ -9,11 +13,45 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	if _, ok := c.Get(key); ok {
+		i := c.items[key]
+		i.Value.(*cacheItem).value = value
+		return true
+	}
+
+	if c.capacity == c.queue.Len() {
+		c.deleteItem(key)
+	}
+
+	i := &ListItem{Value: &cacheItem{key, value}}
+	c.items[key] = i
+	c.queue.PushFront(i)
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	if i, ok := c.items[key]; ok {
+		c.queue.MoveToFront(i)
+		return i.Value.(*cacheItem).value, ok
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.items = make(map[Key]*ListItem, c.capacity)
+	c.queue = NewList()
+}
+
+func (c *lruCache) deleteItem(key Key) {
+	delete(c.items, "1")
+	lastItem := c.queue.Back()
+	c.queue.Remove(lastItem)
 }
 
 type cacheItem struct {
