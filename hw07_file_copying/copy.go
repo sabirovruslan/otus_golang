5084450a650da324f96882bee520a8315f4a6a7f
Ruslan <ruslan.sabirov@alao.ch>
2022-12-17 15:53:54 +0400

    HW07 implementaion

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,53 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	infoInFile, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if infoInFile.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+	if infoInFile.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if limit <= 0 {
+		limit = infoInFile.Size()
+	}
+	if offset < 0 {
+		offset = int64(0)
+	}
+
+	inFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	buf := make([]byte, 1)
+	var currentSize int64
+	for currentSize < limit {
+		n, err := inFile.ReadAt(buf, offset)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		_, err = outFile.Write(buf[:n])
+		if err != nil {
+			return err
+		}
+		currentSize += int64(n)
+		offset += int64(n)
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,77 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"os"
+	"testing"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/suite"
+)
+
+type copySuite struct {
+	suite.Suite
+}
+
+func TestRunCopySuite(t *testing.T) {
+	suite.Run(t, new(copySuite))
+}
+
+func (s *copySuite) TestCopy() {
+	s.Error(Copy("", "", 0, 0))
+	s.EqualError(Copy("testdata/input.txt", "", 0, 0), "open : no such file or directory")
+	s.EqualError(Copy("not_exist.txt", "/tmp/out.txt", 0, 0), "stat not_exist.txt: no such file or directory")
+	s.EqualError(Copy("testdata/input.txt", "/tmp/out.txt", math.MaxInt64, 0), "offset exceeds file size")
+	s.EqualError(Copy("/dev/urandom", "/tmp/out.txt", 0, 0), "unsupported file")
+
+	s.Equal(Copy("testdata/input.txt", "/tmp/out.txt", 0, 0), nil)
+	_, err := os.Stat("/tmp/out.txt")
+	s.Equal(err, nil)
+
+	buf := make([]byte, 20)
+	infoIn, _ := os.Stat("testdata/input.txt")
+
+	s.Equal(Copy("testdata/input.txt", "/tmp/out.txt", 0, 1), nil)
+	infoOut, _ := os.Stat("/tmp/out.txt")
+	s.Equal(infoOut.Size(), int64(1))
+	file, _ := os.Open("/tmp/out.txt")
+	n, _ := file.Read(buf)
+	s.Equal(string(buf[:n]), "G")
+	file.Close()
+
+	s.Equal(Copy("testdata/input.txt", "/tmp/out.txt", 0, 2), nil)
+	infoOut, _ = os.Stat("/tmp/out.txt")
+	s.Equal(infoOut.Size(), int64(2))
+	file, _ = os.Open("/tmp/out.txt")
+	n, _ = file.Read(buf)
+	s.Equal(string(buf[:n]), "Go")
+	file.Close()
+
+	s.Equal(Copy("testdata/input.txt", "/tmp/out.txt", 0, 5), nil)
+	infoOut, _ = os.Stat("/tmp/out.txt")
+	s.Equal(infoOut.Size(), int64(5))
+	file, _ = os.Open("/tmp/out.txt")
+	n, _ = file.Read(buf)
+	s.Equal(string(buf[:n]), "Go\nDo")
+	file.Close()
+
+	s.Equal(Copy("testdata/input.txt", "/tmp/out.txt", 3, 2), nil)
+	infoOut, _ = os.Stat("/tmp/out.txt")
+	s.Equal(infoOut.Size(), int64(2))
+	file, _ = os.Open("/tmp/out.txt")
+	n, _ = file.Read(buf)
+	s.Equal(string(buf[:n]), "Do")
+	file.Close()
+
+	s.Equal(Copy("testdata/input.txt", "/tmp/out.txt", 0, 0), nil)
+	infoOut, _ = os.Stat("/tmp/out.txt")
+	s.Equal(infoOut.Size(), infoIn.Size())
+
+	s.Equal(Copy("testdata/input.txt", "/tmp/out.txt", -1, -1), nil)
+	infoOut, _ = os.Stat("/tmp/out.txt")
+	s.Equal(infoOut.Size(), infoIn.Size())
+
+	s.Equal(Copy("testdata/input.txt", "/tmp/out.txt", infoIn.Size(), 0), nil)
+	infoOut, _ = os.Stat("/tmp/out.txt")
+	s.Equal(infoOut.Size(), int64(0))
+
 }
