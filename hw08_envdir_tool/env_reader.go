a4b65c9a459ea974677a96bd22b770ace8fc130a
Ruslan <ruslan.sabirov@alao.ch>
2022-12-22 23:36:09 +0400

    HW08 implementation envdir reader

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +21,46 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	envs := make(Environment)
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		line, err := readLine(filepath.Join(dir, entry.Name()))
+		if err != nil {
+			return nil, err
+		}
+		line = bytes.Replace(line, []byte("\x00"), []byte("\n"), -1)
+		v := strings.TrimRight(string(line), " ")
+		if len(v) == 0 {
+			envs[entry.Name()] = EnvValue{"", true}
+		} else {
+			envs[entry.Name()] = EnvValue{v, false}
+		}
+	}
+	return envs, nil
+}
+
+func readLine(filePath string) ([]byte, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewReader(file)
+	line, _, err := scanner.ReadLine()
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return line, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,44 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestReadDir(t *testing.T) {
-	// Place your code here
+	"github.com/stretchr/testify/suite"
+)
+
+type envSuite struct {
+	suite.Suite
+}
+
+func TestRunEnvSuite(t *testing.T) {
+	suite.Run(t, new(envSuite))
+}
+
+func (s *envSuite) TestRead() {
+	_, err := ReadDir("")
+	s.EqualError(err, "open : no such file or directory")
+
+	envs, err := ReadDir("testdata/env")
+	s.Equal(err, nil)
+	s.Equal(len(envs), 5)
+
+	envValue := envs["BAR"]
+	s.Equal(envValue.Value, "bar")
+	s.Equal(envValue.NeedRemove, false)
+
+	envValue = envs["FOO"]
+	s.Equal(envValue.Value, "   foo\nwith new line")
+	s.Equal(envValue.NeedRemove, false)
+
+	envValue = envs["UNSET"]
+	s.Equal(envValue.Value, "")
+	s.Equal(envValue.NeedRemove, true)
+
+	envValue = envs["EMPTY"]
+	s.Equal(envValue.Value, "")
+	s.Equal(envValue.NeedRemove, true)
+
+	envValue = envs["HELLO"]
+	s.Equal(envValue.Value, "\"hello\"")
+	s.Equal(envValue.NeedRemove, false)
 }
