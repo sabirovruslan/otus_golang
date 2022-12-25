098dc4c5af071ea8dea12ed85749509d594bbc40
Ruslan <ruslan.sabirov@alao.ch>
2022-12-25 19:50:19 +0400

    HW08 Refactoring

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 	"os/exec"
@@ -8,7 +9,7 @@ import (
 
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	command := exec.Command(cmd[0], cmd[1:]...)
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
 	command.Stdout = os.Stdout
 
 	for k, v := range env {
@@ -28,7 +29,8 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 	}
 
 	if err := command.Wait(); err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
 			return exitErr.ExitCode()
 		}
 		log.Fatalf("Wait run: %v", err)
