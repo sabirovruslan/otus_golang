68cb6465dad02668ec317ceefc656ba48a8211f6
Ruslan <ruslan.sabirov@alao.ch>
2023-01-07 16:21:26 +0400

    HW12 setup logger

--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,20 +1,41 @@
 package logger
 
-import "fmt"
+import (
+	"time"
 
-type Logger struct { // TODO
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func customLevelEncoder(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString("[" + level.String() + "]")
+}
+
+func syslogTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("Jan 2 15:04:05"))
+}
+
+type Logger struct {
+	logger *zap.Logger
 }
 
 func New(level string) *Logger {
-	return &Logger{}
+	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig.LevelKey = level
+	cfg.EncoderConfig.EncodeLevel = customLevelEncoder
+	cfg.EncoderConfig.EncodeTime = syslogTimeEncoder
+	logger, _ := cfg.Build()
+	return &Logger{
+		logger: logger,
+	}
 }
 
 func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+	l.logger.Info(msg)
 }
 
 func (l Logger) Error(msg string) {
-	// TODO
+	l.logger.Error(msg)
 }
 
 // TODO
